2dd9859cfb1251f5b02822563a6a4ddbe5701239
Gareth Evans <garethjevans@users.noreply.github.com>
2019-06-06 11:52:46 +0100

    feat: updated helm to 2.14.1

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 SA=~/.gke_sa.json
-HELM_VERSION=2.12.2
+HELM_VERSION=2.14.1
 
 function install_dependencies() {
 	wget https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
